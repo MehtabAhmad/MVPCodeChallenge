619f5ff75836643304d75b3d0c09f6f67db99d86
Mehtab <work4edu@gmail.com>
2023-01-01 17:09:35 +0000

    Refactor test to remove duplication

--- MoviesFramework/MoviesFrameworkTests/RemoteMovieLoaderTests.swift
+++ MoviesFramework/MoviesFrameworkTests/RemoteMovieLoaderTests.swift
@@ -15,6 +15,7 @@ class RemoteMovieLoader {
         self.client = client
         self.url = url
     }
+    
     func load() {
         client.get(from: url)
     }
@@ -28,25 +29,30 @@ protocol HTTPClient {
 final class RemoteMovieLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://any-url.com")!
-        _ = RemoteMovieLoader(client: client, url: url)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
         let url = URL(string: "https://any-url.com")!
-        let sut = RemoteMovieLoader(client: client, url: url)
+        let (sut,client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
     
-    class HTTPClientSpy:HTTPClient {
-        var requestedURL:URL?
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteMovieLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteMovieLoader(client: client, url: url)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
         
         func get(from url: URL) {
             requestedURL = url
