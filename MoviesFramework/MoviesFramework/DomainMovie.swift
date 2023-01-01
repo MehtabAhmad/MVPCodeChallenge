90564d646f4108e1f77570abb4518b5089d25d7f
Mehtab <work4edu@gmail.com>
2023-01-01 15:54:13 +0000

    create domainMovie model
    This movie model will be agnostic of external platforms  so it can be used with cross platforms like macOs, watchOs, iOS etc

---
+++ MoviesFramework/MoviesFramework/DomainMovie.swift
@@ -0,0 +1,16 @@
+//
+//  DomainMovie.swift
+//  MoviesFramework
+//
+//  Created by Mehtab on 01/01/2023.
+//
+
+import Foundation
+
+struct DomainMovie {
+    let id:UUID
+    let title:String
+    let description:String
+    let poster:URL
+    let rating:Float
+}
