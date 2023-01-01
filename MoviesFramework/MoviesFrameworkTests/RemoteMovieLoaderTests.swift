18e523833e4aeba8c4609e1071829c9ec35adde8
Mehtab <work4edu@gmail.com>
2023-01-01 21:17:18 +0000

    Use explicit JSON data for non 200 HTTP response tests

--- MoviesFramework/MoviesFrameworkTests/RemoteMovieLoaderTests.swift
+++ MoviesFramework/MoviesFrameworkTests/RemoteMovieLoaderTests.swift
@@ -54,7 +54,8 @@ final class RemoteMovieLoaderTests: XCTestCase {
         samples.enumerated().forEach { index, code in
             
             expect(sut, toCompletewith: .failure(.invalidData), when: {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             })
         }
     }
@@ -154,7 +155,7 @@ final class RemoteMovieLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data:Data, at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
