9d7d3887e8771a6627db20da508297d990e52cdf
Mehtab <work4edu@gmail.com>
2023-01-01 17:25:38 +0000

    Guarantee that we can load more than once

--- MoviesFramework/MoviesFrameworkTests/RemoteMovieLoaderTests.swift
+++ MoviesFramework/MoviesFrameworkTests/RemoteMovieLoaderTests.swift
@@ -26,6 +26,16 @@ final class RemoteMovieLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteMovieLoader, client: HTTPClientSpy) {
@@ -36,9 +46,11 @@ final class RemoteMovieLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
