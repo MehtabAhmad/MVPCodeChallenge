70753bcd27786eb425fa4ce2af8a230b0de59006
Mehtab <work4edu@gmail.com>
2023-01-02 18:59:25 +0000

    Add `APIMovie` data transfer representation to decouple the items mapper from `DomainMovie` data models

--- MoviesFramework/MoviesFramework/MovieAPI/MovieItemsMapper.swift
+++ MoviesFramework/MoviesFramework/MovieAPI/MovieItemsMapper.swift
@@ -7,33 +7,26 @@
 
 import Foundation
 
+internal struct APIMovie: Decodable {
+    public let id:Int
+    public let title:String
+    public let overview:String
+    public let poster_path:URL
+    public let vote_average:Float
+}
+
 final class MovieItemsMapper {
     private struct Root: Decodable {
         let results: [APIMovie]
-        var domainMovies: [DomainMovie] {
-            results.map {$0.domainMovie}
-        }
-    }
-    
-    private struct APIMovie: Decodable {
-        public let id:Int
-        public let title:String
-        public let overview:String
-        public let poster_path:URL
-        public let vote_average:Float
-        
-        var domainMovie: DomainMovie {
-            return DomainMovie(id: id, title: title, description: overview, poster: poster_path, rating: vote_average)
-        }
     }
     
     static var OK_200: Int { return 200 }
     
-    static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteMovieLoader.Result {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [APIMovie] {
         guard response.statusCode == OK_200,
-              let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteMovieLoader.Error.invalidData)
+            let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteMovieLoader.Error.invalidData
         }
-        return .success(root.domainMovies)
+        return root.results
     }
 }
--- MoviesFramework/MoviesFramework/MovieAPI/RemoteMovieLoader.swift
+++ MoviesFramework/MoviesFramework/MovieAPI/RemoteMovieLoader.swift
@@ -28,10 +28,26 @@ final public class RemoteMovieLoader: LoadMovieUseCase {
             guard self != nil else { return }
             switch result {
             case let .success(data, response):
-                completion(MovieItemsMapper.map(data, from: response))
+                completion(RemoteMovieLoader.map(data, from: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
         }
+    }
+    
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let apiMovies = try MovieItemsMapper.map(data, from: response)
+            return .success(apiMovies.toDomainMovies())
+        }
+        catch {
+            return .failure(error)
+        }
+    }
+}
+
+private extension Array where Element == APIMovie {
+    func toDomainMovies() -> [DomainMovie] {
+        return map { DomainMovie(id: $0.id, title: $0.title, description: $0.overview, poster: $0.poster_path, rating: $0.vote_average)}
     }
 }
