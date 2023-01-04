ed6d2dde2863b2e7601c1d3ecfcdc3826e2fda84
Mehtab <work4edu@gmail.com>
2023-01-04 21:11:49 +0000

    Move FavouriteMovieLoader into it's own file in production

--- MoviesFramework/MoviesFrameworkTests/LoadFavouriteMovieUseCase/LoadFavouriteMovieUseCaseTests.swift
+++ MoviesFramework/MoviesFrameworkTests/LoadFavouriteMovieUseCase/LoadFavouriteMovieUseCaseTests.swift
@@ -8,36 +8,6 @@
 import XCTest
 import MoviesFramework
 
-class FavouriteMovieLoader:LoadMovieUseCase {
-    
-    let store:MovieStore
-    
-    init(store: MovieStore) {
-        self.store = store
-    }
-    
-    func load(completion:@escaping (LoadMovieResult) -> Void) {
-        store.retrieve() { [weak self] result in
-            guard self != nil else {return}
-            switch result {
-            case .empty:
-                completion(.success([]))
-            case let .found(DTOMovies):
-                completion(.success(DTOMovies.toDomainMovies()))
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        }
-    }
-}
-
-private extension Array where Element == StoreMovieDTO {
-    func toDomainMovies() -> [DomainMovie] {
-        return map {
-            DomainMovie(id: $0.id, title: $0.title, description: $0.description, poster: $0.poster, rating: $0.rating)
-        }
-    }
-}
 
 final class LoadFavouriteMovieUseCaseTests: XCTestCase {
     
