6c12b4f33d3a293587074cdad5d3ced232e2dc38
Mehtab <work4edu@gmail.com>
2023-01-04 22:45:07 +0000

    Move HiddenMoviesLoader to it's own file in production

--- MoviesFramework/MoviesFrameworkTests/LoadHiddenMoviesUseCase/LoadHiddenMoviesUseCaseTests.swift
+++ MoviesFramework/MoviesFrameworkTests/LoadHiddenMoviesUseCase/LoadHiddenMoviesUseCaseTests.swift
@@ -8,38 +8,6 @@
 import XCTest
 import MoviesFramework
 
-public final class HiddenMoviesLoader:LoadMovieUseCase {
-    
-    let store:MovieStore
-    
-    public init(store: MovieStore) {
-        self.store = store
-    }
-    
-    public func load(completion:@escaping (LoadMovieResult) -> Void) {
-        store.retrieve() { [weak self] result in
-            guard self != nil else {return}
-            switch result {
-            case .empty:
-                completion(.success([]))
-            case let .found(DTOMovies):
-                completion(.success(DTOMovies.toDomainMovies()))
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        }
-    }
-}
-
-private extension Array where Element == StoreMovieDTO {
-    func toDomainMovies() -> [DomainMovie] {
-        return map {
-            DomainMovie(id: $0.id, title: $0.title, description: $0.description, poster: $0.poster, rating: $0.rating)
-        }
-    }
-}
-
-
 final class LoadHiddenMoviesUseCaseTests: XCTestCase {
 
     func test_init_doesnotMessageStore() {
