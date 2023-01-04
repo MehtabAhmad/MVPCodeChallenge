7ddbfc9666362bbe973ed6d0759188edf32a31eb
Mehtab <work4edu@gmail.com>
2023-01-04 00:38:10 +0000

    Add favourite movie command should request movie insertion

--- MoviesFramework/MoviesFrameworkTests/FavouriteMovie/AddFavouriteMovieUseCaseTests.swift
+++ MoviesFramework/MoviesFrameworkTests/FavouriteMovie/AddFavouriteMovieUseCaseTests.swift
@@ -6,16 +6,27 @@
 //
 
 import XCTest
+import MoviesFramework
 
 class MovieStore {
-    enum Messages {}
+    enum Messages: Equatable {
+        case insert(DomainMovie)
+    }
     var receivedMessages = [Messages]()
+    
+    func insert(_ movie:DomainMovie) {
+        receivedMessages.append(.insert(movie))
+    }
 }
 
 class AddFavouriteMovieUseCaseHandler {
-    
+    let store:MovieStore
     init(store:MovieStore) {
-        
+        self.store = store
+    }
+    
+    func addFavourite(_ movie:DomainMovie) {
+        store.insert(movie)
     }
 }
 
@@ -25,5 +36,25 @@ final class AddFavouriteMovieUseCaseTests: XCTestCase {
         let store = MovieStore()
         let sut = AddFavouriteMovieUseCaseHandler(store: store)
         XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    func test_addFavourite_requestsMovieInsertion() {
+        let store = MovieStore()
+        let sut = AddFavouriteMovieUseCaseHandler(store: store)
+        let item = uniqueMovieItem()
+        sut.addFavourite(item)
+        XCTAssertEqual(store.receivedMessages, [.insert(item)])
+    }
+    
+    
+    // Mark: - Helper
+    
+    private func uniqueMovieItem() -> DomainMovie {
+        DomainMovie(
+            id: UUID().hashValue,
+            title: "a title",
+            description: "a description",
+            poster: URL(string: "http://a-url.com")!,
+            rating: 3.5)
     }
 }
