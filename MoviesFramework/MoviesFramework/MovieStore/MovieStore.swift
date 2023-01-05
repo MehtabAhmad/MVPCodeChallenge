be93c76ddc10bcee90f8a89f9e482ec548b6b11c
Mehtab <work4edu@gmail.com>
2023-01-05 19:37:08 +0000

    MovieStore moved to it's own file

--- MoviesFramework/MoviesFramework/MovieStore/MovieStore.swift
+++ MoviesFramework/MoviesFramework/MovieStore/MovieStore.swift
@@ -7,22 +7,6 @@
 
 import Foundation
 
-public struct StoreMovieDTO: Equatable {
-    public let id:Int
-    public let title:String
-    public let description:String
-    public let poster:URL
-    public let rating:Float
-    
-    public init(id: Int, title: String, description: String, poster: URL, rating: Float) {
-        self.id = id
-        self.title = title
-        self.description = description
-        self.poster = poster
-        self.rating = rating
-    }
-}
-
 public enum RetrieveFavouriteMovieResult {
     case empty
     case found([StoreMovieDTO])
