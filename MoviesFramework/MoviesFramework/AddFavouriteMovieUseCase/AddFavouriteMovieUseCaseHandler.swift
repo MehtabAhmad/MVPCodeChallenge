5288435339c2fca56a86880fe35a36b811536ed8
Mehtab <work4edu@gmail.com>
2023-01-07 22:45:28 +0000

    Update AddFavouriteMovieUseCaseTests and AddFavouriteMovieUseCaseHandler to handle favourite movie already exists use-case
    - Should retrieve existing favourite movies first
    - Delivers error if existing data retrieval fails
    - Delivers error if movie already added in favourites
    - Request insertion if previously no movie inserted in favourites
    - Request insertion if previous favourites exist but new movie does not exist in that list
    - Delivers error on insertion error
    - Succeeds on successful insertion
    - Does not request insertion if SUT instance deallocated
    - Does not deliver insertion result if SUT instance deallocated

--- MoviesFramework/MoviesFramework/AddFavouriteMovieUseCase/AddFavouriteMovieUseCaseHandler.swift
+++ MoviesFramework/MoviesFramework/AddFavouriteMovieUseCase/AddFavouriteMovieUseCaseHandler.swift
@@ -9,14 +9,29 @@ import Foundation
 
 public final class AddFavouriteMovieUseCaseHandler: AddFavouriteMovieUseCase {
     
-    let store:FavouriteMoviesStore
+    private let store:FavouriteMoviesStore
+    private let dataAlreadyExistsError = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "This movie already existis in favourites"])
     
     public init(store:FavouriteMoviesStore) {
         self.store = store
     }
     
     public func addFavourite(_ movie:DomainMovie, completion:@escaping (addFavouriteResult) -> Void) {
-        store.insertFavourite(movie.toDTOMovie()) { [weak self] error in
+        store.retrieveFavourite() { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .found(existingFavourites) where existingFavourites.contains(movie.toDTOMovie()):
+                completion(self.dataAlreadyExistsError)
+            case let .failure(error):
+                completion(error)
+            default:
+                self.insert(movie.toDTOMovie(), completion: completion)
+            }
+        }
+    }
+    
+    private func insert(_ movie:StoreMovieDTO, completion:@escaping (addFavouriteResult) -> Void) {
+        store.insertFavourite(movie) { [weak self] error in
             guard self != nil else { return }
             completion(error)
         }
--- MoviesFramework/MoviesFrameworkTests/AddFavouriteMovieUseCase/AddFavouriteMovieUseCaseTests.swift
+++ MoviesFramework/MoviesFrameworkTests/AddFavouriteMovieUseCase/AddFavouriteMovieUseCaseTests.swift
@@ -9,38 +9,80 @@ import XCTest
 import MoviesFramework
 
 final class AddFavouriteMovieUseCaseTests: XCTestCase {
-
+    
     func test_init_doesnotMessageStore() {
         let (_,store) = makeSUT()
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
     
-    func test_addFavourite_requestsMovieInsertion() {
+    func test_addFavourite_requestsExistingDataRetrieval() {
         let (sut,store) = makeSUT()
         let items = uniqueMovieItems()
         sut.addFavourite(items.model) { _ in }
-        XCTAssertEqual(store.receivedMessages, [.insertFavourite(items.dto)])
+        XCTAssertEqual(store.receivedMessages, [.retrieveFavourite])
     }
+    
+    func test_addFavourite_deliversErrorOnExistingDataRetrievalFailure() {
+        let (sut,store) = makeSUT()
+        let retrievalError = anyNSError()
+        expect(sut, toCompleteWithError: retrievalError, when: {
+            store.completeFavouriteRetrival(with: retrievalError)
+        })
+    }
+    
+    func test_addFavourite_deliversInsertionErrorIfItemAlreadyExists() {
+        let (sut,store) = makeSUT()
+        let dataAlreadyExistsError = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "This movie already existis in favourites"])
+        let existingItem = uniqueMovieItems()
+        expect(sut, toCompleteWithError: dataAlreadyExistsError, when: {
+            store.completeFavouriteRetrival(with: [existingItem.dto])
+        }, item: existingItem.model)
+    }
+    
+    func test_addFavourite_requestInsertionOnEmptyExistingData() {
+        let (sut,store) = makeSUT()
+        let item = uniqueMovieItems()
+        sut.addFavourite(item.model) { _ in}
+        store.completeFavouriteRetrival(with: [])
+        XCTAssertEqual(store.receivedMessages, [.retrieveFavourite, .insertFavourite(item.dto)])
+    }
+    
+    func test_addFavourite_requestInsertionOnNonEmptyExistingDataButItemNotAddedAlready() {
+        let (sut,store) = makeSUT()
+        let oldItem1 = uniqueMovieItems()
+        let oldItem2 = uniqueMovieItems()
+        let newitem = uniqueMovieItems()
+        
+        sut.addFavourite(newitem.model) { _ in}
+        
+        store.completeFavouriteRetrival(with: [oldItem1.dto, oldItem2.dto])
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieveFavourite, .insertFavourite(newitem.dto)])
+    }
+    
     
     func test_addFavourite_deliversErrorOnInsertionError() {
         let (sut,store) = makeSUT()
         
         let insertionError = anyNSError()
         expect(sut, toCompleteWithError: insertionError, when: {
+            store.completeFavouriteRetrival(with: [])
             store.completeFavouriteInsertion(with: insertionError)
         })
-  
     }
+    
     
     func test_addFavourite_succeedsOnSuccessfulInsertion() {
         let (sut,store) = makeSUT()
-       
+        
         expect(sut, toCompleteWithError: nil, when: {
+            store.completeFavouriteRetrival(with: [])
             store.completeFavouriteInsertionSuccessfully()
         })
     }
     
-    func test_addFavourite_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
+    
+    func test_addFavourite_doesNotRequestInsertionAfterSUTInstanceHasBeenDeallocated() {
         let store = FavouriteMoviesStoreSpy()
         var sut:AddFavouriteMovieUseCaseHandler? = AddFavouriteMovieUseCaseHandler(store: store)
         
@@ -48,11 +90,24 @@ final class AddFavouriteMovieUseCaseTests: XCTestCase {
         sut?.addFavourite(uniqueMovieItem()) {
             receivedResults.append($0)
         }
+        sut = nil
+        store.completeFavouriteRetrival(with: [])
+        XCTAssertEqual(store.receivedMessages, [.retrieveFavourite])
+    }
+    
+    
+    func test_addFavourite_doesNotDeliverInsertionResultAfterSUTInstanceHasBeenDeallocated() {
+        let store = FavouriteMoviesStoreSpy()
+        var sut:AddFavouriteMovieUseCaseHandler? = AddFavouriteMovieUseCaseHandler(store: store)
         
+        var receivedResults:[AddFavouriteMovieUseCaseHandler.addFavouriteResult]?
+        sut?.addFavourite(uniqueMovieItem()) {
+            receivedResults?.append($0)
+        }
+        store.completeFavouriteRetrival(with: [])
         sut = nil
         store.completeFavouriteInsertion(with: anyNSError())
-        
-        XCTAssertTrue(receivedResults.isEmpty)
+        XCTAssertNil(receivedResults)
     }
     
     
@@ -66,20 +121,21 @@ final class AddFavouriteMovieUseCaseTests: XCTestCase {
         return (sut,store)
     }
     
-    private func expect(_ sut: AddFavouriteMovieUseCase, toCompleteWithError expectedError:NSError?, when action:() -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    private func expect(_ sut: AddFavouriteMovieUseCase, toCompleteWithError expectedError:NSError?, when action:() -> Void, item:DomainMovie = uniqueMovieItem(), file: StaticString = #filePath, line: UInt = #line) {
         
         var receivedError: Error?
-
+        
         let exp = expectation(description: "Wait for completion")
-        sut.addFavourite(uniqueMovieItem()) { error in
+        sut.addFavourite(item) { error in
             receivedError = error
             exp.fulfill()
         }
-
+        
         action()
-
+        
         wait(for: [exp], timeout: 1.0)
-
+        
         XCTAssertEqual(receivedError as NSError?, expectedError, file: file, line: line)
     }
+    
 }
