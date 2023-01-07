d36530b0e28295ec1ac465d9ff6e6cf6ad522bc5
Mehtab <work4edu@gmail.com>
2023-01-07 17:50:35 +0000

    move FavouriteMovieStore and HiddenMovieStore implementations to their own files

--- MoviesFramework/MoviesFramework/MovieStore/Infrastructure/CoreData/CoreDataMoviesStore.swift
+++ MoviesFramework/MoviesFramework/MovieStore/Infrastructure/CoreData/CoreDataMoviesStore.swift
@@ -18,65 +18,8 @@ public final class CoreDataMoviesStore {
         context = container.newBackgroundContext()
     }
     
-    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
-            let context = self.context
-            context.perform { action(context) }
-        }
-}
-
-extension CoreDataMoviesStore:FavouriteMoviesStore {
-    
-    public func insertFavourite(_ movie: StoreMovieDTO, completion: @escaping favouriteInsertionCompletion) {
-        perform { context in
-            do {
-                let managedFavourite = ManagedFavouriteMovie(context: context)
-                managedFavourite.movie = ManagedFavouriteMovie.managedMovie(from: movie, in: context)
-                try context.save()
-                completion(nil)
-            } catch {
-                completion(error)
-            }
-        }
-    }
-    
-    public func retrieveFavourite(completion: @escaping favouriteRetrievalCompletion) {
-        perform { context in
-            do {
-                let favourites = try ManagedFavouriteMovie.find(in: context)
-                guard favourites.count > 0 else { return completion(.empty) }
-                completion(.found( favourites.map { $0.DTOFavourite }))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
-}
-
-
-extension CoreDataMoviesStore:HiddenMoviesStore {
-    
-    public func insertHidden(_ movie: StoreMovieDTO, completion: @escaping hiddenInsertionCompletion) {
-        perform { context in
-            do {
-                let managedHidden = ManagedHiddenMovie(context: context)
-                managedHidden.movie = ManagedHiddenMovie.managedMovie(from: movie, in: context)
-                try context.save()
-                completion(nil)
-            } catch {
-                completion(error)
-            }
-        }
-    }
-    
-    public func retrieveHidden(completion: @escaping hiddenRetrievalCompletion) {
-        perform { context in
-            do {
-                let hiddens = try ManagedHiddenMovie.find(in: context)
-                guard hiddens.count > 0 else { return completion(.empty) }
-                completion(.found( hiddens.map { $0.DTOFavourite }))
-            } catch {
-                completion(.failure(error))
-            }
-        }
+    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
     }
 }
