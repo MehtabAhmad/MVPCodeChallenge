651c8d90fa3c089055a9d4ca5b43819decd18ab9
Mehtab <work4edu@gmail.com>
2023-01-11 21:32:50 +0000

    Add MovieIOSAppTests target

---
+++ MoviesFramework/MoviesIOSAppTests/SearchMoviesViewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesIOSAppTests.swift
+//  MoviesIOSAppTests
+//
+//  Created by Mehtab on 11/01/2023.
+//
+
+import XCTest
+
+final class SearchMoviesViewControllerTests: XCTestCase {
+
+
+}
