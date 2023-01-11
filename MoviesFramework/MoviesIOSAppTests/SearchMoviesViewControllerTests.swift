02a6ead84e4b954b54c71d4b053d04e3ef450243
Mehtab <work4edu@gmail.com>
2023-01-11 23:55:47 +0000

    extract SUT creation by helper method
    - Instantiate ViewController from storyboard
    - Changed constructor injection to property injection
    - Add memory leak tracking

--- MoviesFramework/MoviesIOSAppTests/SearchMoviesViewControllerTests.swift
+++ MoviesFramework/MoviesIOSAppTests/SearchMoviesViewControllerTests.swift
@@ -12,17 +12,34 @@ import MoviesIOSApp
 final class SearchMoviesViewControllerTests: XCTestCase {
 
     func test_init_doesNotInvokeSearch() {
-        let loader = LoaderSpy()
-        let _ = SearchMoviesViewController(loader:loader)
+        let (_,loader) = makeSUT()
         XCTAssertEqual(loader.searchCallCount, 0)
     }
     
     func test_viewDidLoad_doesNotInvokeSearch() {
-        let loader = LoaderSpy()
-        let _ = SearchMoviesViewController(loader:loader)
+        let (_,loader) = makeSUT()
         XCTAssertEqual(loader.searchCallCount, 0)
     }
+    
+    func test_searchFiedlDelegates_shouldBeConnected() {
+        let (sut,_) = makeSUT()
+        sut.loadViewIfNeeded()
+        XCTAssertNotNil(sut.searchBar.delegate, "SearchField delegates")
+    }
 
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut:SearchMoviesViewController, loader:LoaderSpy) {
+        let loader = LoaderSpy()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sut = storyboard.instantiateViewController(
+        identifier: String(describing: SearchMoviesViewController.self)) as! SearchMoviesViewController
+        sut.moviesLoader = loader
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        return(sut,loader)
+    }
     
     private class LoaderSpy:LoadMovieUseCase {
         
