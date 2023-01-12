41b93f3d88a4307d6e41a5a0fe01d46fe3d9be73
Mehtab <work4edu@gmail.com>
2023-01-12 11:46:36 +0000

    Add insertion messages to clear the context

--- MoviesFramework/MoviesIOSAppTests/SearchMoviesViewControllerTests.swift
+++ MoviesFramework/MoviesIOSAppTests/SearchMoviesViewControllerTests.swift
@@ -14,7 +14,7 @@ final class SearchMoviesViewControllerTests: XCTestCase {
     
     func test_viewDidLoad_doesNotInvokeSearch() {
         let (_,loader) = makeSUT()
-        XCTAssertEqual(loader.searchCallCount, 0)
+        XCTAssertEqual(loader.searchCallCount, 0, "Expect no search request when view is loaded")
     }
     
     func test_searchFiedlDelegates_shouldBeConnected() {
@@ -26,52 +26,49 @@ final class SearchMoviesViewControllerTests: XCTestCase {
         let (sut,loader) = makeSUT()
         let emptyText = ""
         sut.simulateUserInitiatedSearch(with: emptyText)
-        XCTAssertEqual(loader.searchCallCount, 0)
+        XCTAssertEqual(loader.searchCallCount, 0, "Expect no search request when search text is empty")
     }
     
     func test_userInitiatedSearch_invokeSearchWhenSearchFieldIsNotEmpty() {
         let (sut,loader) = makeSUT()
         sut.simulateUserInitiatedSearch()
-        XCTAssertEqual(loader.searchCallCount, 1)
+        XCTAssertEqual(loader.searchCallCount, 1, "Expect a search request when user initiated search with some search text")
     }
     
     func test_viewDidLoad_doesNotShowLoadingIndicator() {
         let (sut,_) = makeSUT()
-        XCTAssertFalse(sut.isShowingLoadingIndicator)
+        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expect no loading indicator when view loads")
     }
     
     func test_userInitiatedSearch_showsLoadingIndicator() {
         let (sut,_) = makeSUT()
         sut.simulateUserInitiatedSearch()
-        XCTAssertTrue(sut.isShowingLoadingIndicator)
+        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expect loading indicator when when user initiated a search request")
     }
     
     func test_userInitiatedSearch_hideLoadingIndicatorOnCompletion() {
         let (sut,loader) = makeSUT()
         
         sut.simulateUserInitiatedSearch()
-        XCTAssertTrue(sut.isShowingLoadingIndicator)
+        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expect loading indicator when when user initiated a search request")
         
         loader.completeLoading()
-        XCTAssertFalse(sut.isShowingLoadingIndicator)
+        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expect no loading indicator when when user initiated search request has been completed")
     }
     
     func test_userInitiatedSearch_onlyInvokeSearchRequestWhenNoPreviousSearchRunning() {
         let (sut,loader) = makeSUT()
         
         sut.simulateUserInitiatedSearch()
-        XCTAssertEqual(loader.searchCallCount, 1)
+        XCTAssertEqual(loader.searchCallCount, 1, "Expect a search request when user initiated a search")
         
         sut.simulateUserInitiatedSearch()
-        XCTAssertEqual(loader.searchCallCount, 1)
-        
-        sut.simulateUserInitiatedSearch()
-        XCTAssertEqual(loader.searchCallCount, 1)
+        XCTAssertEqual(loader.searchCallCount, 1, "Expect no new request when user initiated search before completing the last request")
         
         loader.completeLoading()
         
         sut.simulateUserInitiatedSearch()
-        XCTAssertEqual(loader.searchCallCount, 2)
+        XCTAssertEqual(loader.searchCallCount, 2, "Expect a new request when user initiated search after completing the last request")
     }
     
 
